227388b39d1a53fa3597480370361f5b12579e9a
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-04 10:40:00 +0530

    declare helper functions

--- data-structures/linear/Array/array.c
+++ data-structures/linear/Array/array.c
@@ -5,10 +5,9 @@
 //declaring a static integer array
 int int_arr[SIZE];
 
-
-
-//defining display function
-void display();
+void display();//declaring display function
+int* dynamic_array(int size);//declaring dynamic array function
+void display_array(int length, int* array);//declaring function to display array
 
 int main(){
 
