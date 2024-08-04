6679aba1b6496a1acda1eaaf7f042e76ae7a2959
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-04 10:50:35 +0530

    define display function

--- data-structures/linear/Array/array.c
+++ data-structures/linear/Array/array.c
@@ -5,7 +5,7 @@
 //declaring a static integer array
 int int_arr[SIZE];
 
-void display();//declaring display function
+int display();//declaring display function
 int* dynamic_array(int size);//declaring dynamic array function
 void display_array(int length, int* array);//declaring function to display array
 
@@ -14,5 +14,18 @@ int main(){
     
 
     return 0;
+}
+
+int display(){
+    int selection;
+    printf("----basic array demonstration----\n");
+    printf("\n1.add elements to static array");
+    printf("\n2.create dynamic array");
+    printf("\n3.display static array");
+    printf("\n4.display dynamic array");
+    printf("\n5.retrieve elements using index");
+    printf("\n6.exit");
+    printf("\n\nselect: ");
+    scanf("%d",&selection);
 }
 
