c10f93082a7324ed87af6da34c1609d5af5d899c
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-04 14:40:26 +0530

    add display function

--- data-structures/linear/Array/array.c
+++ data-structures/linear/Array/array.c
@@ -3,11 +3,13 @@
 #define SIZE 10
 
 int* allocate_memory(int size, int* arr);
-
+void display_array(int* arr, int start, int size);
 
 
 int main(){
-   
+    int Static_Array[SIZE];
+    int* dynamic_array = NULL;
+    
 
     return 0;
 }
@@ -16,10 +18,16 @@ int main(){
 
 int* allocate_memory(int size, int* arr){
     if(arr == NULL){
-        int* new_array = (int*)malloc(size*sizeof(int));
-        return new_array;
+        arr = (int*)malloc(size*sizeof(int));
+        return arr;
     }else{
         arr = (int*)realloc(arr,size);
         return arr;
+    }
+}
+
+void display_array(int* arr, int start, int size){
+    for(int i = start; i<size; i++){
+        printf("[%d] at index-> %d\n",arr[i],i);
     }
 }
