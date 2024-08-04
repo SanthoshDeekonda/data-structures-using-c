56dae05c39f9178159e78d10dff1bd9cf8514cf0
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-04 15:17:49 +0530

    update case 1

--- data-structures/linear/Array/array.c
+++ data-structures/linear/Array/array.c
@@ -5,11 +5,39 @@
 int* allocate_memory(int size, int* arr);
 void display_array(int* arr, int start, int size);
 int display_home();
+int array_selection();
 
+int selection,d_size,n_ele,user_choice,proceed = 1;
 
 int main(){
     int Static_Array[SIZE];
     int* dynamic_array = NULL;
+    while(proceed){
+        selection = display_home();
+        switch(selection){
+            case 1:
+                if(dynamic_array == NULL){
+                    printf("\nEnter the size of the array: ");
+                    scanf("%d",&d_size);
+                    dynamic_array = allocate_memory(d_size,dynamic_array);
+                    n_ele = 0;
+                    printf("\nMemory Allocation successful Array of size %d created", d_size);
+                }else{
+                    printf("\ndynamic array of size %d already created do you want to reallocate memory?(0/1) ", d_size);
+                    scanf("%d",&user_choice);
+                    if(user_choice == 1){
+                        printf("\nEnter size grater then the previous size %d: ",d_size);
+                        scanf("%d",&d_size);
+                        dynamic_array = allocate_memory(d_size,dynamic_array);
+                        printf("\nrellocation successful");
+                    }
+                }
+                break;
+            case 2:
+                
+                
+        }
+    }
     
 
     return 0;
@@ -42,6 +70,13 @@ int display_home(){
     printf("\n5.exit");
     printf("\nSelect option: ");
     scanf("%d",&user_selection);
+    system('cls');
 
     return user_selection;
+}
+
+int array_selection(){
+    int arr;
+    printf("\n1.static Array\n2.Dynamic array\nChoice: ");
+    scanf("%d", &arr);
 }
