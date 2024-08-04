b5812af8d6ea5d44566a36c4a4e44119d4dcbe5e
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-04 14:51:28 +0530

    add display home function

--- data-structures/linear/Array/array.c
+++ data-structures/linear/Array/array.c
@@ -4,6 +4,7 @@
 
 int* allocate_memory(int size, int* arr);
 void display_array(int* arr, int start, int size);
+int display_home();
 
 
 int main(){
@@ -30,4 +31,17 @@ void display_array(int* arr, int start, int size){
     for(int i = start; i<size; i++){
         printf("[%d] at index-> %d\n",arr[i],i);
     }
+}
+
+int display_home(){
+    int user_selection;
+    printf("\n1.create dynamic array");
+    printf("\n2.insert elements");
+    printf("\n3.access with index");
+    printf("\n4.display");
+    printf("\n5.exit");
+    printf("\nSelect option: ");
+    scanf("%d",&user_selection);
+
+    return user_selection;
 }
