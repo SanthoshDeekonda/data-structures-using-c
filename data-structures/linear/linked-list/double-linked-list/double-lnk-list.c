790cde8c1b9e25a4720921bc318cc76b00e023ab
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-08 23:17:31 +0530

    add create node and init function

--- data-structures/linear/linked-list/double-linked-list/double-lnk-list.c
+++ data-structures/linear/linked-list/double-linked-list/double-lnk-list.c
@@ -4,6 +4,7 @@
 
 typedef struct Block{
     int data;
+    int pos;
     struct Block* next;
     struct Block* prev;
 }Node;
@@ -12,8 +13,30 @@ typedef struct DoubleLinkedList{
     Node* head;
 }DoubleLinkedList;
 
+
+DoubleLinkedList* init_double_list();
+Node* createNode(int data);
+
 int main(){
 
 
     return 0;
 }
+
+DoubleLinkedList* init_double_list(){
+    DoubleLinkedList* new_list = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));
+    new_list->head = NULL;
+
+    return new_list;
+};
+
+Node* createNode(int data){
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    new_node->pos = 0;
+
+    return new_node;
+}
+
