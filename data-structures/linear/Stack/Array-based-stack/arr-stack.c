22733b02a8a3edf64c551459d33de6f5d778101e
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-09 17:06:52 +0530

    add push function

--- data-structures/linear/Stack/Array-based-stack/arr-stack.c
+++ data-structures/linear/Stack/Array-based-stack/arr-stack.c
@@ -1,14 +1,36 @@
-#include <stdlib.h>
-#define SIZE 10
+#include <stdio.h>
+#include <stdbool.h>
+#define SIZE 5
 
 
 
 
-int STACK[SIZE];
+int STACK[SIZE],top = -1;
+bool push(int* stack, int data);
+bool isEmpty(int top);
+bool isFull(int top);
+
 
 int main(){
 
 
-
     return 0;
+}
+
+bool isEmpty(int top){
+    return top == -1;
+}
+
+bool isFull(int top){
+    return top == SIZE - 1;
+}
+
+bool push(int* stack, int data){
+    if(!isFull(top)){
+        stack[++top] = data;
+        return true;
+    }else{
+        printf("\nStack overflow");
+        return false;
+    }
 }
