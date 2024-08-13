dbd9926822601973f26e4970a21592b7ea7d769b
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-13 20:37:27 +0530

    add init_stack function

--- data-structures/linear/Stack/linked-list-based-stack/lnk-list-stack.c
+++ data-structures/linear/Stack/linked-list-based-stack/lnk-list-stack.c
@@ -12,9 +12,20 @@ typedef struct stack{
     Node* top;
 }Stack;
 
+Stack* init_stack();
+
 
 int main(){
 
+    Stack* mystack = init_stack();
+   
 
     return 0;
+}
+
+Stack* init_stack(){
+    Stack* new_stack = (Stack*)malloc(sizeof(Stack));
+    new_stack->top = NULL;
+
+    return new_stack;
 }
