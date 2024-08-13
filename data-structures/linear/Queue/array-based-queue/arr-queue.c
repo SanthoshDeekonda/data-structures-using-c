7717e6cb908b056c8d781d5ca48a78420813a6e9
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-13 21:50:47 +0530

    add isfull isempty function

--- data-structures/linear/Queue/array-based-queue/arr-queue.c
+++ data-structures/linear/Queue/array-based-queue/arr-queue.c
@@ -3,11 +3,25 @@
 #define SIZE 5
 
 
-int Queue[SIZE];
+int Queue[SIZE],front = -1,top = -1;
+
+bool isEmpty();
+bool isFull();
+
 
 int main(){
 
+    printf("%d", isEmpty());
+    printf("%d", isFull());
 
 
     return 0;
+}
+
+bool isEmpty(){
+    return front == -1 && top == -1;
+}
+
+bool isFull(){
+    return top == (SIZE - 1);
 }
